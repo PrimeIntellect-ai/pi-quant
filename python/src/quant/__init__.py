39fc90ffa454cacb995ae9f7018c29dd2ef4716f
Sami Jaghouar <sami.jaghouar@gmail.com>
2025-02-17 21:21:12 +0000

    add __all__
    Signed-off-by: Sami Jaghouar <sami.jaghouar@gmail.com>

--- python/src/quant/__init__.py
+++ python/src/quant/__init__.py
@@ -3,4 +3,6 @@ __author__ = 'Mario Sieg'
 __email__ = 'mario.sieg.64@gmail.com'
 __author_email__ = 'mario.sieg.64@gmail.com'
 
-from ._quant import *
+from ._quant import Context, RoundMode
+
+__all__ = ['Context', 'RoundMode']
