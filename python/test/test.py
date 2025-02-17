1639ca1c70831c519982839a7867df7be44e6a2e
Neo <mario.sieg.64@gmail.com>
2025-02-17 13:33:13 +0100

    Add CI

---
+++ python/test/test.py
@@ -0,0 +1,14 @@
+import torch
+import quant
+
+def test_quant_int8():
+    tensor = torch.rand(8192 * 8192 * 32)
+    print(tensor)
+
+    ctx = quant.Context()
+    quantized_tensor = torch.empty(tensor.numel(), dtype=torch.uint8)
+    scale = 0.00784
+    zero_point = 128
+    ctx.quant_uint8(tensor.data_ptr(), quantized_tensor.data_ptr(), numel=tensor.numel(), scale=scale,
+                    zero_point=zero_point, mode=quant.RoundMode.NEAREST)
+    print(quantized_tensor)
