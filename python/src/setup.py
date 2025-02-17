6ffc8337b8430adf2f17219542d79f6563ddf44a
Sami Jaghouar <sami.jaghouar@gmail.com>
2025-02-17 17:48:44 +0000

    use pyproject.toml
    Signed-off-by: Sami Jaghouar <sami.jaghouar@gmail.com>

--- python/src/setup.py
+++ python/src/setup.py
@@ -62,23 +62,8 @@ class CMakeBuildExecutor(build_ext):
                 ['cmake', '--build', '.'] + build_args, cwd=self.build_temp
             )
         )
-
-
-# Setup dependencies from requirements.txt
-lib_folder = os.path.dirname(os.path.realpath(__file__))
-requirement_path = f'{lib_folder}/requirements.txt'
-install_requires = []
-if os.path.isfile(requirement_path):
-    with open(requirement_path) as f:
-        install_requires = f.read().splitlines()
-
+        
 setup(
-    name='fast-quant',
-    version='0.1.0',
-    author='Mario Sieg',
-    author_email='mario.sieg.64@gmail.com',
-    description='Multithreaded SIMD int8 and int4 quantization kernels.',
-    long_description='Multithreaded SIMD int8 and int4 quantization kernels.',
     packages=['quant'],
     package_data={
         'quant': ['*.dylib', '*.so', '*.dll'],
@@ -88,6 +73,5 @@ setup(
     cmdclass={
         'build_ext': CMakeBuildExecutor,
     },
-    zip_safe=False,
-    install_requires=install_requires,
+    zip_safe=False
 )
